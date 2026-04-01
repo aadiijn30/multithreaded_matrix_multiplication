04a94675f0c1a7c23d87bbd0662d23100b601720
aadiijn30 <aadiijn30@gmail.com>
2026-04-01 19:46:22 +0530

    Update matrix3.cpp

--- matrix3.cpp
+++ matrix3.cpp
@@ -8,16 +8,16 @@ using namespace std;
 
 void matrix_multiplication(int x_rows,int x_columns,int y_rows,int y_columns,vector<int>&x,vector<int>&y,vector<int>&z,int start,int end)
 { 
-for(int r=start;r<end;++r)
-{
-for(int c=0;c<x_columns;++c)
-{
-for(int c1=0;c1<y_columns;++c1)
-{
-z[r*y_columns+c1]+=(x[r*x_columns+c])*(y[c*y_columns+c1]);
-}
-}
-}
+  for(int r=start;r<end;++r)
+  {
+    for(int c=0;c<x_columns;++c)
+    {
+      for(int c1=0;c1<y_columns;++c1)
+      {
+        z[r*y_columns+c1]+=(x[r*x_columns+c])*(y[c*y_columns+c1]);
+      }
+    }
+  }
 }
 
 
@@ -47,13 +47,12 @@ else batch_size=(x_rows/cores);
 int s=0,e;
 for(int i=0;i<cores;i++)
 {
-e=s+batch_size;
-if(e>x_rows) e=x_rows;
-t.push_back(thread(matrix_multiplication,x_rows,x_columns,y_rows,y_columns,ref(x),ref(y),ref(z),s,e));
-s+=batch_size;
+  e=s+batch_size;
+  if(e>x_rows) e=x_rows;
+  t.push_back(thread(matrix_multiplication,x_rows,x_columns,y_rows,y_columns,ref(x),ref(y),ref(z),s,e));
+  s+=batch_size;
 }
 for(auto &it:t) it.join();
-
 
 auto end=chrono::high_resolution_clock::now();
 auto diff=end-start;
